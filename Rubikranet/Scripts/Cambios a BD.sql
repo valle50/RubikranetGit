325ef2a715ce1d30293e0560dca1aa57f58aeb0b
CHRIS <chrisvalle21@gmail.com>
2018-07-26 12:07:46 -0500

    Actualización de catálogos

--- Rubikranet/Scripts/Cambios a BD.sql
+++ Rubikranet/Scripts/Cambios a BD.sql
@@ -5,7 +5,7 @@
 --Se cambió el tipo de dato bigint por varchar(15) para lo teléfonos
 ALTER TABLE empleados ALTER COLUMN telefono varchar(15) NULL
 --Se agregaron campo para verificar la accesibilidad a los miebros familiares de acuerdo a su edad
-alter table areas add codigo_edad_accesibilidad char(1) null
+alter table areas add codigo_edad_accesibilidad bit null
 --se agregó campo para verificar el estatus del área
 alter table areas add estatus bit null
 --se agregó campo para quitar acceso a áreas a membresías.
