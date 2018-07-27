e483048a6cfefdfa2068f52a69ccb2816fe34489
CHRIS <chrisvalle21@gmail.com>
2018-07-27 11:52:55 -0500

    commit prueba

--- Rubikranet/Scripts/Cambios a BD.sql
+++ Rubikranet/Scripts/Cambios a BD.sql
@@ -2,7 +2,7 @@
 --Estos cambios vendrán incluidos en la BD final, por el momento la BD actual es beta
 --mientras se siga desarrollando la aplicación.
 
---Se cambió el tipo de dato bigint por varchar(15) para lo teléfonos
+--Se cambió el tipo de dato bigint por varchar(15) para los teléfonos
 ALTER TABLE empleados ALTER COLUMN telefono varchar(15) NULL
 --Se agregaron campo para verificar la accesibilidad a los miebros familiares de acuerdo a su edad
 alter table areas add codigo_edad_accesibilidad bit null
@@ -12,3 +12,4 @@ alter table areas add estatus bit null
 alter table accesibilidad_areas add estatus bit null
 --se agregó campo para ocultar categorias de membresías
 alter table categorias_membresias add estatus_visible bit null
+
