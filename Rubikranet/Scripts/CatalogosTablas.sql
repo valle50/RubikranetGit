64b63d141ed396a4329a34ff276531c5e39e05c0
ACER-PC <ACER-PC@ACER-PC>
2018-07-31 19:34:01 -0500

    Catálogo estados

--- Rubikranet/Scripts/CatalogosTablas.sql
+++ Rubikranet/Scripts/CatalogosTablas.sql
@@ -113,3 +113,40 @@ Usuario				Consulta solo la información del establecimiento
 Encargados de área  Consulta la información de cada una de sus áreas      
 */
 
+/*
+Catálogo estados
+
+id_estado		nombre_estado
+1               Aguascalientes	
+2				Baja California	
+3				Baja California Sur 
+4				Campeche	
+5				Chiapas	
+6				Chihuahua	
+7				Ciudad de México
+8				Coahuila	
+9				Colima	
+10				Durango	
+11				Guanajuato	
+12			    Guerrero	
+13				Hidalgo	
+14				Jalisco	
+15				México	
+16				Michoacán	
+17				Morelos	
+18				Nayarit	
+19				Nuevo León	
+20				Oaxaca	
+21				Puebla	
+22				Querétaro	
+23				Quintana Roo	
+24				San Luis Potosí	
+25				Sinaloa	
+26				Sonora
+27				Tabasco	
+28				Tamaulipas	
+29				Tlaxcala	
+30				Veracruz	
+31				Yucatán	
+32				Zacatecas
+*/
