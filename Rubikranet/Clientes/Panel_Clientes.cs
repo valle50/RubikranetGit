22a6652475f643dfd03b41a5d828e8d2e69f01ea
valle50 <chrisvalle21@gmail.com>
2018-08-03 02:30:58 -0500

    Se agregaron controles de usuario a carpeta de clientes

---
+++ Rubikranet/Clientes/Panel_Clientes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rubikranet.Clientes
+{
+    public partial class Panel_Clientes : UserControl
+    {
+        public Panel_Clientes()
+        {
+            InitializeComponent();
+        }
+        public static Panel_Clientes Instancia = new Panel_Clientes();
+
+        private void ManipulaContenido(object a, object b)
+        {
+            var controlUsuario = a as UserControl;
+
+            if (!panelclientes.Controls.Contains(controlUsuario))
+            {
+                panelclientes.Controls.Clear();
+                panelclientes.Controls.Add(controlUsuario);
+            }
+            else
+            {
+                panelclientes.Controls.Remove(controlUsuario);
+                panelclientes.BackgroundImage = Properties.Resources.fondo3;
+            }
+
+        }
+
+        private void btnclientes_Click(object sender, EventArgs e)
+        {
+            ManipulaContenido(clientes.Instancia, sender);
+            btnclientes.BackColor = Color.SkyBlue;
+        }
+    }
+}
