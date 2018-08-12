98fdefb4a88f060cebd408f0c9d6bd8285af74a2
Slashlml <slash_bl@outlook.com>
2018-08-12 17:56:34 -0500

    design

--- Rubikranet/Clientes/Panel_Clientes.cs
+++ Rubikranet/Clientes/Panel_Clientes.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Rubikranet.Funciones;
 
 namespace Rubikranet.Clientes
 {
@@ -21,7 +22,18 @@ namespace Rubikranet.Clientes
 
         private void Panel_Clientes_Load(object sender, EventArgs e)
         {
-
+            Conexion.Paginar(string.Format("select * from CLIENTES_MEMBRESIAS order by num desc"),"DataMember1", 20);
+            Funcion.CargaTablaDatos_Estilos_Botones(tablaClientes);
+            Actualizar();
+            tablaClientes.Columns[2].Visible = false;
+            tablaClientes.Columns[3].Visible = false;
+        }
+        private void Actualizar()
+        {
+            lblTotalReg.Text = Conexion.countRow().ToString();
+            lblPagActual.Text = Conexion.numPag().ToString();
+            lblTotalPag.Text = Conexion.countPag().ToString();
+            Funcion.AjustaContenido(tablaClientes);
         }
     }
 }
